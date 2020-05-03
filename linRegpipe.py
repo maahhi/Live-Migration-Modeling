7d2e3fb5de1f128cac333a97a26fbf11461a9e46
maahhi <mahi_jfg@rocketmail.com>
2020-05-03 15:02:01 +0430

    simple pipeline with linear regression for one technique and one metric. leaning curve and errors. and one sample randomforest model

---
+++ linRegpipe.py
@@ -0,0 +1,206 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error,mean_absolute_error, explained_variance_score, r2_score
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.model_selection import learning_curve
+
+
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html#sphx-glr-auto-examples-model-selection-plot-learning-curve-py
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    axes : array of 3 axes, optional (default=None)
+        Axes to use for plotting the curves.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,#,scoring="neg_mean_squared_error"
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
+
+
+df = pd.read_csv('data.csv')
+
+tech_uniq = df.technique.unique()
+df_tech =df.loc[df['technique'] == tech_uniq[0]].copy()
+
+# features and target
+X_fields = ['VM_size','pdr','working_set_pages','wse','nwse','mwpp', #vm fitures
+           'vm_perf_instructions','max-bandwidth','SRC_cpu_system','SRC_cpu_user' ,'SRC_net_manage_ifutil',#Source host
+            'SRC_cpu_system','SRC_cpu_user','DST_cpu_system','DST_cpu_user','SRC_net_manage_ifutil','DST_net_manage_ifutil',#Src + dest host
+            'RPTR', 'THR_benefit', 'DTC_benefit', 'DLTC_benefit', 'POST_benefit', 'ewss', 'enwss'#composed (nwss not found)
+            ]
+y_fields = ['TT']
+
+# shuffling and split
+mask = np.random.rand(len(df_tech)) < 0.8
+train = df_tech[mask]
+X_train_raw = train[X_fields].values
+y_train = train[y_fields].values
+test = df_tech[~mask]
+X_test_raw = test[X_fields].values
+y_test = test[y_fields].values
+
+X_raw = df_tech[X_fields].values
+y = df_tech[y_fields].values
+
+# normalization
+min_max_scaler = preprocessing.MinMaxScaler()
+X_train = min_max_scaler.fit_transform(X_train_raw)
+X_test = min_max_scaler.fit_transform(X_test_raw)
+X = min_max_scaler.fit_transform(X_raw)
+#train
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) #training the algorithm
+
+#predict
+y_pred = regressor.predict(X_test)
+
+'''
+#visualize
+
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+df1 = df.head(25)
+df1.plot(kind='bar',figsize=(16,10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+plt.scatter(X_test[:,2], y_test,  color='gray')
+plt.scatter(X_test[:,2], y_pred, color='red', linewidth=2)
+plt.show()
+'''
+
+# error
+MSE = mean_squared_error(y_test, y_pred)
+MAE = mean_absolute_error(y_test, y_pred)
+EVS = explained_variance_score(y_test, y_pred)
+R2 = r2_score(y_test, y_pred)
+print('MSE ', MSE, '\n MAE ', MAE, '\n EVS ', EVS, '\n R2', R2)
+
+
+plot_learning_curve(regressor, "linear", X_train, y_train)
+
+
+
+# another model
+model = RandomForestRegressor(n_estimators=10, max_features=2)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# error
+MSE = mean_squared_error(y_test, y_pred)
+MAE = mean_absolute_error(y_test, y_pred)
+EVS = explained_variance_score(y_test, y_pred)
+R2 = r2_score(y_test, y_pred)
+print('MSE ',MSE,'\n MAE ',MAE,'\n EVS ',EVS,'\n R2',R2)
+
+plot_learning_curve(model, "RandomForestRegressor", X_train, y_train)
+
+plt.show()
+
